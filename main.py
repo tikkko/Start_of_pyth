40cfeb30f2b4199b9eab0170e25ce058a1b99618
tikkko <tix.yefimenko@gmail.com>
2022-11-14 15:35:31 +0200

    3d Commit

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-#a = '3'
-#b = '4'
-#if a > b:
-#    print (a)
-#else: print (b)
 
-a = '2'
-b = '3'
-c = '4'
+def compar (a, b):
+   if a > b:
+      print(a)
+   else: print(b)
 
-if c < a < b
-    elif c < b:
-    print c
+def least (a, b, c):
+    if b > a < c:
+       print('a is less to b and c')
+    elif a > b < c:
+       print('b is less to a and c')
+    elif b > c < a:
+       print('c is less to b and a')
+    else:
+       pass
 
-elif a < c:
-    print
+def module (x):
+    x = abs(x)
+    return x
 
+def suma (a, b):
+    c = a + b
+    print(c)
+
+def positive (n):
+    if n > 0:
+        print('number is positive')
+    elif n == 0:
+        print('ERROR - zero is neutral number')
+    elif n < 0:
+        print('number is negative')
+
+
+if __name__ == '__main__':
+   # compar(2, 3)
+   # least(3, 4, 5)
+   # modul(-10)
+   # sum(2,3)
+   # positive(0)
